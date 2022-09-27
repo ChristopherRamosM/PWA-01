845f39cb0bfed8129afc3ee47050a52e88a04a8a
ChristopherRamosM <chjeramosmo@ittepic.edu.mx>
2022-09-27 13:16:23 -0600

    promesa-4
    Finalización

--- promesa-4.js
+++ promesa-4.js
@@ -1,9 +1,6 @@
 //CR: funcion normal que regresa una promesa pero el resultado se obtiene mas lento.
-function fcnSumarLento( numero ){
+/*function fcnSumarLento( numero ){
     return new Promise(function(resolve, reject){
-        /*if(numero >= 5 ){
-            reject('Sumar Lento Fallo');
-        }*/
         setTimeout(function(){
             resolve( numero + 1 );
             }, 800);
@@ -24,4 +21,45 @@ let fcnSumarRapido = (numero)=>{
 Promise.race([fcnSumarLento(5), fcnSumarRapido(10)])
     .then(respuesta =>{
         console.log('Respuestas:', respuesta);
+    });
+*/
+
+//CR: funcion normal que regresa una promesa pero el resultado se obtiene mas lento.
+function fcnSumarLento( numero ){
+    return new Promise(function(resolve, reject){
+        setTimeout(function(){
+            resolve( numero + 1 );
+            }, 800);
+    });
+}
+
+//CR: esta es la manera mas optimizada de definir una funcion pero de tipo arrow (flecha) que tambien regresa una promesa
+//pero el resultado se obtiene mas rapido.
+/*let fcnSumarRapido = (numero)=>{
+    return new Promise(function(resolve, reject){
+        setTimeout(function(){
+            //resolve( numero + 1 );
+            reject('Error en la promesa de la funcion Sumar Rapido');
+            }, 300);
+    });
+}*/
+
+//CR: esta es la manera mas optimizada de definir una funcion pero de tipo arrow (flecha) que tambien regresa una promesa 
+//pero el resultado se obtiene mas rapido.
+let fcnSumarRapido = (numero)=>{
+    return new Promise(function(resolve, reject){
+        setTimeout(function(){
+            //resolve( numero + 1 );
+            reject('Error en la promesa de la funcion Sumar Rapido');
+            }, 1000);
+    });
+}
+
+//CR: Promise.race
+Promise.race([fcnSumarLento(5), fcnSumarRapido(10)])
+    .then(respuesta =>{
+        console.log('Respuesta:', respuesta);
+    })
+    .catch(error=>{
+        console.log("Error en la respuesta de la promesa: ", error);
     });
