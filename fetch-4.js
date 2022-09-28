c893b0b85e030cbaae83e4f209da5ea7c8430aa0
ChristopherRamosM <chjeramosmo@ittepic.edu.mx>
2022-09-27 19:08:56 -0600

    fetch-4 y carpeta Images
    Creación de carpeta Images y modificación de fetch-4

--- fetch-4.js
+++ fetch-4.js
@@ -1,5 +1,5 @@
 //CR: Fetch con codigo de promesa simplificada.
-fetch('Images/coche.png') 
+/*fetch('Images/coche.png') 
 .then(resp => resp.blob()) 
 .then(image => { 
     console.log(image); 
@@ -12,4 +12,29 @@ fetch('Images/carro-deportivo.png')
 }) 
 .then(image => { 
     console.log(image); 
+});*/
+
+//CR: instanciamos los dos componentes de imagen de la pagina index.html 
+let img1 = document.getElementById("IdImg1");
+let img2 = document.getElementById("IdImg2");
+
+//CR: Fetch con codigo de promesa simplificada.
+fetch('Images/coche.png')
+.then(resp => resp.blob())
+.then(image => {
+    console.log(image);
+    //FIC: instancia para imagen 1
+    var imgPath1 = URL.createObjectURL(image);
+    img1.src = imgPath1;
+});
+//FIC: Fetch con codigo de promesa normal.
+fetch('Images/carro-deportivo.png')
+.then(resp => {
+    return resp.blob();
+})
+.then(image => {
+    console.log(image);
+    //FIC: instancia para imagen 2
+    var imgPath2 = URL.createObjectURL(image);
+    img2.src = imgPath2;
 });
